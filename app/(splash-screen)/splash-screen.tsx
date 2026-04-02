bd25c9d8865a9b6680882f44feb51240aa443123
rTaha01 <fiverr01help@gmail.com>
2026-04-02 14:39:57 +0500

    feat: implement authentication and main app screens with custom Tailwind CSS styles and remove legacy assets

---
+++ app/(splash-screen)/splash-screen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const splashScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-accent">
+      <Text className="text-xl font-bold text-background">splash-screen</Text>
+    </View>
+  )
+}
+
+export default splashScreen
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,31 @@
 import "@/global.css";
 import { Text, View } from "react-native";
- 
+import { Link } from "expo-router";
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-accent">
         Welcome to Nativewind!
       </Text>
+      <Link
+        href="/(splash-screen)/splash-screen"
+        className="mt-4 rounded-full bg-accent px-4 py-2 text-xl font-bold text-background"
+      >
+        Go to Splash Screen
+      </Link>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 rounded-full bg-accent px-4 py-2 text-xl font-bold text-background"
+      >
+        Go to Sign In
+      </Link>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 rounded-full bg-accent px-4 py-2 text-xl font-bold text-background"
+      >
+        Go to Sign Up
+      </Link>
     </View>
   );
 }
